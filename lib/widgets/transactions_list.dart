b00aad993c4e3ea4b57de8c508ea854fb3c650b1
Thanos <thanasakis@gmail.com>
2020-10-28 14:52:38 +0100

    Added listview with builder that recycles

--- lib/widgets/transactions_list.dart
+++ lib/widgets/transactions_list.dart
@@ -9,50 +9,56 @@ class TransactionsList extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: _transactions.map((transaction) {
-        return Card(
-            child: Row(
-              children: [
-                Container(
-                  // left right, top bottom symmetric
-                  margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Colors.purple,
-                      width: 2,
+    return Container(
+      height: 300,
+      child: ListView.builder(
+        itemCount: _transactions.length,
+        itemBuilder: (context, itemIndex) {
+          final transaction = _transactions[itemIndex];
+
+          return Card(
+              child: Row(
+                children: [
+                  Container(
+                    // left right, top bottom symmetric
+                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        color: Colors.purple,
+                        width: 2,
+                      ),
                     ),
-                  ),
-                  //for all directions
-                  padding: EdgeInsets.all(10),
-                  child: Text(
-                    "\$${transaction.amount}",
-                    style: TextStyle(
-                        fontSize: 20,
-                        fontWeight: FontWeight.bold,
-                        color: Colors.purple),
-                  ),
-                ),
-                Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      transaction.title,
+                    //for all directions
+                    padding: EdgeInsets.all(10),
+                    child: Text(
+                      "\$${transaction.amount}",
                       style: TextStyle(
-                          fontSize: 16,
+                          fontSize: 20,
                           fontWeight: FontWeight.bold,
-                          color: Colors.black),
+                          color: Colors.purple),
                     ),
-                    Text(
-                      // DateFormat('yyyy-MM-dd').format(transaction.date),
-                      DateFormat.yMMMd().format(transaction.date),
-                      style: TextStyle(fontSize: 12, color: Colors.grey),
-                    ),
-                  ],
-                )
-              ],
-            ));
-      }).toList(),
+                  ),
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        transaction.title,
+                        style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.black),
+                      ),
+                      Text(
+                        // DateFormat('yyyy-MM-dd').format(transaction.date),
+                        DateFormat.yMMMd().format(transaction.date),
+                        style: TextStyle(fontSize: 12, color: Colors.grey),
+                      ),
+                    ],
+                  )
+                ],
+              ));
+        },
+      ),
     );
   }
 }
