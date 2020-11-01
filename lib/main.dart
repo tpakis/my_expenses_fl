93b4efe553977b88121631ffc7a2217e73abce2d
Thanos <thanasakis@gmail.com>
2020-11-01 23:30:26 +0100

    add custom image, add empty view  seperator, empty list placeholder

--- lib/main.dart
+++ lib/main.dart
@@ -26,13 +26,13 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final List<Transaction> _transactions = [
-    Transaction(
+    /*Transaction(
         id: 't1', title: 'New Shoes', amount: 69.99, date: DateTime.now()),
     Transaction(
         id: 't2',
         title: 'Weekly Groceries',
         amount: 16.53,
-        date: DateTime.now())
+        date: DateTime.now())*/
   ];
 
   void _addTransaction(String title, double amount) {
--- lib/widgets/transactions_list.dart
+++ lib/widgets/transactions_list.dart
@@ -11,51 +11,68 @@ class TransactionsList extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       height: 300,
-      child: ListView.builder(
-        itemCount: _transactions.length,
-        itemBuilder: (context, itemIndex) {
-          final transaction = _transactions[itemIndex];
+      child: _transactions.isEmpty
+          ? Column(
+              children: [
+                Text(
+                  "No transactions added!",
+                  style: Theme.of(context).textTheme.title,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Container(
+                    height: 200,
+                    child: Image.asset(
+                      "assets/images/waiting.png",
+                      fit: BoxFit.cover,
+                    )),
+              ],
+            )
+          : ListView.builder(
+              itemCount: _transactions.length,
+              itemBuilder: (context, itemIndex) {
+                final transaction = _transactions[itemIndex];
 
-          return Card(
-              child: Row(
-                children: [
-                  Container(
-                    // left right, top bottom symmetric
-                    margin: EdgeInsets.symmetric(vertical: 10, horizontal: 15),
-                    decoration: BoxDecoration(
-                      border: Border.all(
-                        color: Theme.of(context).primaryColor,
-                        width: 2,
+                return Card(
+                    child: Row(
+                  children: [
+                    Container(
+                      // left right, top bottom symmetric
+                      margin:
+                          EdgeInsets.symmetric(vertical: 10, horizontal: 15),
+                      decoration: BoxDecoration(
+                        border: Border.all(
+                          color: Theme.of(context).primaryColor,
+                          width: 2,
+                        ),
+                      ),
+                      //for all directions
+                      padding: EdgeInsets.all(10),
+                      child: Text(
+                        "\$${transaction.amount.toStringAsFixed(2)}",
+                        style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                            color: Theme.of(context).primaryColor),
                       ),
                     ),
-                    //for all directions
-                    padding: EdgeInsets.all(10),
-                    child: Text(
-                      "\$${transaction.amount.toStringAsFixed(2)}",
-                      style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                          color: Theme.of(context).primaryColor),
-                    ),
-                  ),
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        transaction.title,
-                        style: Theme.of(context).textTheme.title
-                      ),
-                      Text(
-                        // DateFormat('yyyy-MM-dd').format(transaction.date),
-                        DateFormat.yMMMd().format(transaction.date),
-                        style: TextStyle(fontSize: 12, color: Colors.grey),
-                      ),
-                    ],
-                  )
-                ],
-              ));
-        },
-      ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(transaction.title,
+                            style: Theme.of(context).textTheme.title),
+                        Text(
+                          // DateFormat('yyyy-MM-dd').format(transaction.date),
+                          DateFormat.yMMMd().format(transaction.date),
+                          style: TextStyle(fontSize: 12, color: Colors.grey),
+                        ),
+                      ],
+                    )
+                  ],
+                ));
+              },
+            ),
     );
   }
 }
