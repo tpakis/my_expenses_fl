f89145845466bca6a90efec7d3b7d12638e2fea0
Thanos <thanasakis@gmail.com>
2020-12-01 23:02:42 +0100

    account for keyboard size, add scrollview to avoid keyboard on top

--- lib/widgets/new_transaction.dart
+++ lib/widgets/new_transaction.dart
@@ -19,7 +19,9 @@ class _NewTransactionState extends State<NewTransaction> {
   void _submitData() {
     double amount = double.tryParse(_amountController.text);
 
-    if (_titleController.text.isEmpty || amount == null || _selectedDate == null) {
+    if (_titleController.text.isEmpty ||
+        amount == null ||
+        _selectedDate == null) {
       return;
     }
     widget.addTransactionCallback(_titleController.text, amount, _selectedDate);
@@ -47,57 +49,65 @@ class _NewTransactionState extends State<NewTransaction> {
 
   @override
   Widget build(BuildContext context) {
-    return Card(
-      child: Container(
-        padding: EdgeInsets.all(10),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            TextField(
-              decoration: InputDecoration(labelText: "Title"),
-              controller: _titleController,
-            ),
-            TextField(
-              decoration: InputDecoration(labelText: "Amount"),
-              controller: _amountController,
-              keyboardType: TextInputType.numberWithOptions(decimal: true),
-              onSubmitted: (_) {
-                _submitData();
-              },
-            ),
-            Container(
-              height: 70,
-              child: Row(
-                children: [
-                  Expanded( // take all the available space
-                    child: Text(
-                      _selectedDate == null
-                          ? "No Date Chosen!"
-                          : "Picked Date ${DateFormat.yMd().format(_selectedDate)}",
-                    ),
-                  ),
-                  FlatButton(
-                    onPressed: () {
-                      _presentDatePicker();
-                    },
-                    textColor: Theme.of(context).primaryColor,
-                    child: Text(
-                      "Choose Date",
-                      style: TextStyle(fontWeight: FontWeight.bold),
-                    ),
-                  )
-                ],
+    return SingleChildScrollView(
+      child: Card(
+        child: Container(
+          padding: EdgeInsets.only(
+            left: 10,
+            right: 10,
+            top: 10,
+            bottom: MediaQuery.of(context).viewInsets.bottom + 10,
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              TextField(
+                decoration: InputDecoration(labelText: "Title"),
+                controller: _titleController,
               ),
-            ),
-            RaisedButton(
-              onPressed: () {
-                _submitData();
-              },
-              child: Text("Add Transaction"),
-              color: Theme.of(context).primaryColor,
-              textColor: Theme.of(context).textTheme.button.color,
-            )
-          ],
+              TextField(
+                decoration: InputDecoration(labelText: "Amount"),
+                controller: _amountController,
+                keyboardType: TextInputType.numberWithOptions(decimal: true),
+                onSubmitted: (_) {
+                  _submitData();
+                },
+              ),
+              Container(
+                height: 70,
+                child: Row(
+                  children: [
+                    Expanded(
+                      // take all the available space
+                      child: Text(
+                        _selectedDate == null
+                            ? "No Date Chosen!"
+                            : "Picked Date ${DateFormat.yMd().format(_selectedDate)}",
+                      ),
+                    ),
+                    FlatButton(
+                      onPressed: () {
+                        _presentDatePicker();
+                      },
+                      textColor: Theme.of(context).primaryColor,
+                      child: Text(
+                        "Choose Date",
+                        style: TextStyle(fontWeight: FontWeight.bold),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+              RaisedButton(
+                onPressed: () {
+                  _submitData();
+                },
+                child: Text("Add Transaction"),
+                color: Theme.of(context).primaryColor,
+                textColor: Theme.of(context).textTheme.button.color,
+              )
+            ],
+          ),
         ),
       ),
     );
